4d5dcfe2b714a67a6f2c81d056a9baf1f55a9017
chiefeh <gregory.bennett@gmail.com>
2020-11-08 15:15:05 +0000

    moved the route53 zoneid to github actions secret

--- route53.tf
+++ route53.tf
@@ -1,5 +1,5 @@
 resource "aws_route53_record" "chiefeh-digital" {
-  zone_id = "Z09953346UVXNI7YWBKZ"
+  zone_id = env.route53_zoneid
   name = "my-first-iac.chiefeh.digital"
   type = "A"
   ttl = "300"
